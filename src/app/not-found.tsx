8a3019a8855d9fc0613b70ea1b889d7a7d7e64fb
musiur <musiur.opu@gmail.com>
2024-11-20 11:45:56 +0600

    update loading page in not-found

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -15,14 +15,12 @@ export default function NotFound() {
   }, []);
   return (
     <div className="flex flex-col items-center justify-center h-screen">
-      <h2 className="text-2xl font-bold">Not Found</h2>
-      <p className="text-lg">Could not find requested resource</p>
       <Link
         href={`/${_language}`}
         className="text-blue-500 underline flex items-center gap-2"
       >
         <Sun className="w-4 h-4 animate-spin" />
-        Return Home
+        Loading...
       </Link>
     </div>
   );

2a6bcec57cc1214b68e19593cb57deb9f41bf5df
musiur <musiur.opu@gmail.com>
2024-11-20 09:43:48 +0600

    add navbar

---
+++ src/components/layout/navbar.tsx
@@ -0,0 +1,19 @@
+import { Button } from "../ui/button";
+
+const Navbar = () => {
+  return (
+    <header>
+      <nav>
+        <div>Navbar</div>
+        <ul>
+          <li>Menu</li>
+        </ul>
+        <div>
+          <Button>Login</Button>
+        </div>
+      </nav>
+    </header>
+  );
+};
+
+export default Navbar;

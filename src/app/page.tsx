fb42d45e41557447fdfd18ac3174bd5954ba3464
musiur <musiur.opu@gmail.com>
2024-11-20 09:38:46 +0600

    refactor layout.tsx and heading-one class in globals.css

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ const Page = () => {
   return (
     <div className="container">
       <div className="section">
-        <h1 className="heading-one">Home page হোম পেইজ</h1>
+        <h1 className="heading-one">Eelevate হোম পেইজ</h1>
       </div>
     </div>
   );

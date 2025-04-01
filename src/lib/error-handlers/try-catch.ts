645ce362fd7f9294123b8cb0baa1cd07430534ee
musiur <musiur.opu@gmail.com>
2025-04-01 20:01:56 +0600

    upgrade try-catch utils error handler

--- src/lib/error-handlers/try-catch.ts
+++ src/lib/error-handlers/try-catch.ts
@@ -1,3 +1,9 @@
+
+/**
+ * type T is for Type of Data which gonna be dynamic and passed when the tryCatch gonna be invocked
+ * type F is for type of Error whcih gonna be thrown by system when the tryCatch gonna be invocked
+ */
+
 type Success<T> = {
     data: T,
     error: null
@@ -11,7 +17,10 @@ type Failer<F> = {
 
 type Result<T, F = Error> = Success<T> | Failer<F>;
 
-
+/**
+ * @param promise 
+ * @returns { data: T | null, error: F | null}
+ */
 export const tryCatch = async <T, F = Error>(
     promise: Promise<T>
 ): Promise<Result<T, F>> => {
